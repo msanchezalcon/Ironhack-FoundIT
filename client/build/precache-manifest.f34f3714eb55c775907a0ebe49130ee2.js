d22cbdee59e34392bc12552b1c9f2c880f75d16b
Míriam Sánchez Alcón <msanchezalcon.msa@gmail.com>
2020-07-31 12:44:51 +0200

    deploy2

---
+++ client/build/precache-manifest.f34f3714eb55c775907a0ebe49130ee2.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3342ff14edf4341d1fa587b798ce0bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "796fa4ee47eb40aa2310",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "60113e6e2810e00e0117",
+    "url": "/static/css/main.23ee80e9.chunk.css"
+  },
+  {
+    "revision": "796fa4ee47eb40aa2310",
+    "url": "/static/js/2.5b88f3c1.chunk.js"
+  },
+  {
+    "revision": "50ebf9df8e516f397c4565cccb79e50d",
+    "url": "/static/js/2.5b88f3c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60113e6e2810e00e0117",
+    "url": "/static/js/main.954f6bf4.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  },
+  {
+    "revision": "a55222a6f12388c09750d496d9583b52",
+    "url": "/static/media/app.a55222a6.svg"
+  },
+  {
+    "revision": "7859fe34491b99d456c9b3a82b5f85b8",
+    "url": "/static/media/chat.7859fe34.svg"
+  },
+  {
+    "revision": "fe3ad0b8e1f7f1beeda676943bfd19ce",
+    "url": "/static/media/edit.fe3ad0b8.svg"
+  },
+  {
+    "revision": "d87809f3165bbc08139c8dad0cfd6057",
+    "url": "/static/media/exit.d87809f3.svg"
+  },
+  {
+    "revision": "bbbaa41ee058c9876a44cf5bfa12f2e8",
+    "url": "/static/media/inbox.bbbaa41e.svg"
+  },
+  {
+    "revision": "384152e1cbf5187e1983f7e753c9efd2",
+    "url": "/static/media/lupa.384152e1.svg"
+  },
+  {
+    "revision": "eec7822348078d9253ffb867776641a5",
+    "url": "/static/media/trash.eec78223.svg"
+  }
+]);
